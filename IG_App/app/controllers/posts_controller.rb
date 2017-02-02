cf8065dca62381247e7147ce7412f22dfe4bffeb
Frontlines 3 <cliftonh02@gmail.com>
2017-02-02 10:04:17 -0500

    Semi Complete

--- IG_App/app/controllers/posts_controller.rb
+++ IG_App/app/controllers/posts_controller.rb
@@ -4,8 +4,20 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
-  def show
+  def create
+    @post = Post.new[permit_post]
+    if @post.save
+      flash[:success]= "Success"
+      redirect_to post_path(@post)
+    else
+      flash[:error]= @post.errors.full_messages
+      redirect_to new_post_path
   end
 end
+
+  def show
+    @post = Post.find(params[:id])
+  end
